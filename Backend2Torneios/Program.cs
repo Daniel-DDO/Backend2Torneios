c151119e2d2390d81c954dc9835ce21c7f0ce29b
Daniel DDO <dandionisio70@gmail.com>
2026-02-17 10:27:12 -0300

    Atualização

--- Backend2Torneios/Program.cs
+++ Backend2Torneios/Program.cs
@@ -5,12 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-var dbHost = Environment.GetEnvironmentVariable("DB_URL");
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
 var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
 var dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD");
-var dbName = "defaultdb";
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 
-var connectionString = $"Host={dbHost};Database={dbName};Username={dbUser};Password={dbPass};Ssl Mode=Require;Trust Server Certificate=true";
+var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass};Ssl Mode=Require;Trust Server Certificate=true";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
@@ -24,10 +25,15 @@ using (var scope = app.Services.CreateScope())
     {
         var context = services.GetRequiredService<AppDbContext>();
         context.Database.EnsureCreated();
+        Console.WriteLine("Conectado ao banco com sucesso!");
     }
     catch (Exception ex)
     {
         Console.WriteLine($"Erro ao conectar no banco: {ex.Message}");
+        if (ex.InnerException != null)
+        {
+            Console.WriteLine($"Detalhe do erro: {ex.InnerException.Message}");
+        }
     }
 }
 
--- Backend2Torneios/obj/Debug/net10.0/Backend2Torneios.AssemblyInfo.cs
+++ Backend2Torneios/obj/Debug/net10.0/Backend2Torneios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend2Torneios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8669eee4c94e102e77d9f5aee7e5844f32483a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend2Torneios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend2Torneios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
